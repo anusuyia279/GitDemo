8091a05a545f98b40f868adcb4a4a7cc3810df45
Anusuyia <anujasper279@gmail.com>
2024-04-14 15:24:49 +0530

    commit on develop branch by demo

--- src/test/java/GitPractice/Example3.java
+++ src/test/java/GitPractice/Example3.java
@@ -4,5 +4,6 @@ public class Example3 {
     public static void main(String args[])
     {
         System.out.println("first statement in  3");
+        System.out.println("new line in 3");
     }
 }
