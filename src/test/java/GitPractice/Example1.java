40774fa71a1491ac12f57e2c4c443452bc9ceea1
Anusuyia <anujasper279@gmail.com>
2024-04-14 13:18:49 +0530

    User1 made the changes 1

--- src/test/java/GitPractice/Example1.java
+++ src/test/java/GitPractice/Example1.java
@@ -5,11 +5,14 @@ public class Example1 {
     {
 
         System.out.println("My first statement in example 1");
-<<<<<<< HEAD
-=======
+
         System.out.println("My first statement in example 2");
         System.out.println("My first statement in example 3");
->>>>>>> 621170b729724d7d21acbcff0da6344fef9cf1f2
+
+        // User 1 to Demo
+
+        System.out.println("User1 made the changes");
+
 
     }
 }
