ed2b7848f7e531324bd01321fe3a9491cf32ba72
Anusuyia <anujasper279@gmail.com>
2024-04-14 15:39:16 +0530

    Commit by user1 on develop in exmaple2

--- src/test/java/GitPractice/Example2.java
+++ src/test/java/GitPractice/Example2.java
@@ -6,5 +6,7 @@ public class Example2 {
     {
         System.out.println("My first statement in example 2");
         System.out.println("Changes in Example2 by User1");
+        int a=10;
+
     }
 }
