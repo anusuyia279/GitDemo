2a04135188e15713325b33817db860390dd76e3e
Anusuyia <anujasper279@gmail.com>
2024-04-14 13:14:15 +0530

    Demo to User1

--- src/test/java/GitPractice/Example1.java
+++ src/test/java/GitPractice/Example1.java
@@ -3,7 +3,13 @@ package GitPractice;
 public class Example1 {
     public static void main (String args[])
     {
+
         System.out.println("My first statement in example 1");
+<<<<<<< HEAD
+=======
+        System.out.println("My first statement in example 2");
+        System.out.println("My first statement in example 3");
+>>>>>>> 621170b729724d7d21acbcff0da6344fef9cf1f2
 
     }
 }
