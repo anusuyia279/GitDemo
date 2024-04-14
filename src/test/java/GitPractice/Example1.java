621170b729724d7d21acbcff0da6344fef9cf1f2
Anusuyia <anujasper279@gmail.com>
2024-04-14 12:44:00 +0530

    Commit by User2 Demo

--- src/test/java/GitPractice/Example1.java
+++ src/test/java/GitPractice/Example1.java
@@ -3,7 +3,11 @@ package GitPractice;
 public class Example1 {
     public static void main (String args[])
     {
+
         System.out.println("My first statement in example 1");
+        System.out.println("My first statement in example 2");
+        System.out.println("My first statement in example 3");
+
     }
 }
 
