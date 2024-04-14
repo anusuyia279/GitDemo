5d57a9740590b1bcb75464f1b86fc9020d5fde39
Anusuyia <anujasper279@gmail.com>
2024-04-14 12:15:38 +0530

    My First commit

---
+++ src/test/java/GitPractice/Example1.java
@@ -0,0 +1,9 @@
+package GitPractice;
+
+public class Example1 {
+    public static void main (String args[])
+    {
+        System.out.println("My first statement in example 1");
+    }
+}
+
---
+++ src/test/java/GitPractice/Example2.java
@@ -0,0 +1,9 @@
+package GitPractice;
+
+public class Example2 {
+
+    public static void main(String args[])
+    {
+        System.out.println("My first statement in example 2");
+    }
+}
